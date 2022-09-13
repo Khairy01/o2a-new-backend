08b259c5a738c7e0cf03cb82b1cb3aaf1d374266
Khairy01 <skhairy@ept.sn>
2022-09-13 11:58:23 +0000

    modifInConfig

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
-
 module.exports = {
     // All imported modules in your tests should be mocked automatically
     // automock: false,
@@ -18,26 +17,28 @@ module.exports = {
     clearMocks: true,
   
     // Indicates whether the coverage information should be collected while executing the test
-    // collectCoverage: false,
+     collectCoverage: true,
   
     // An array of glob patterns indicating a set of files for which coverage information should be collected
-    // collectCoverageFrom: null,
+     collectCoverageFrom: ["**/*.{js,jsx}",
+         "!**/node_modules/**",
+         "!**/vendor/**"],
   
     // The directory where Jest should output its coverage files
-    // coverageDirectory: null,
+        coverageDirectory: './src/coverage',
   
     // An array of regexp pattern strings used to skip coverage collection
-    // coveragePathIgnorePatterns: [
-    //   "/node_modules/"
-    // ],
+     coveragePathIgnorePatterns: [
+      "/node_modules/"
+     ],
   
     // A list of reporter names that Jest uses when writing coverage reports
-    // coverageReporters: [
-    //   "json",
-    //   "text",
-    //   "lcov",
-    //   "clover"
-    // ],
+     coverageReporters: [
+      "json",
+      "text",
+       "lcov",
+       "clover"
+     ],
   
     // An object that configures minimum threshold enforcement for coverage results
     // coverageThreshold: null,
--- src/__tests__/environment.js
+++ src/__tests__/environment.js
@@ -3,9 +3,9 @@ const NodeEnvironment = require('jest-environment-node');
 const MemoryDatabaseServer = require('../config/MemoryDatabaseServer');
 
 class CustomEnvironment extends NodeEnvironment {
+
   async setup() {
     await super.setup();
-
     this.global.__DB_URL__ = await MemoryDatabaseServer.getConnectionString();
   }
 
